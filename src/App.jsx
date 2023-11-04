b775b42a7b78aa62de21b18d10b361e91d8ed0c1
Limerque <126677156+Limerque@users.noreply.github.com>
2023-11-04 12:01:35 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from "react";
+import LoveCalculator from "./components/LoveCalculator";
+
+export default function App() {
+  return (
+    <main>
+      <LoveCalculator />
+    </main>
+  );
+}
